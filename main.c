0a7651b595f0e6c85e8eb15702e14f28edecfa44
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-07-02 06:46:09 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,7 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Tugas Masa Depan dalam mata kuliah
+Algoritma dan Struktur Data untuk keberkahan-Nya,
+maka saya tidak akan melakukan kecurangan seperti
+yang telah di spesifikasikan Aamiin.*/
+#include "tmdasd25.h" // include header "tmdasd25.h"
+int main(){slv return 0;} // call macro slv and solve the program
